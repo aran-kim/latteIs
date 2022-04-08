9c6db691ab2c8966318f0095ca89d9a7b611b2eb
KimSangYub <ct06@naver.com>
2022-04-08 14:13:30 +0900

    Initial commit

---
+++ src/main/java/site/LatteIs/latteIs/LatteIsApplication.java
@@ -0,0 +1,13 @@
+package site.LatteIs.latteIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatteIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatteIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/LatteIs/latteIs/LatteIsApplicationTests.java
@@ -0,0 +1,13 @@
+package site.LatteIs.latteIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatteIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
