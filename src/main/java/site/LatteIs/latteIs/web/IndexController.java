324e1f0503347765cae946c86ec5d8aade4cd24e
KimSangYub <ct06@naver.com>
2022-04-12 14:47:31 +0900

    login update login show error

--- src/main/java/site/LatteIs/latteIs/web/IndexController.java
+++ src/main/java/site/LatteIs/latteIs/web/IndexController.java
@@ -4,18 +4,33 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import site.LatteIs.latteIs.config.auth.LoginUser;
+import site.LatteIs.latteIs.config.auth.dto.SessionUser;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    //private final PostsService postsService;
+    private final HttpSession httpSession;
+
     @GetMapping("/")
     public String index(Model model){
+
         return "index";
     }
 
-    @GetMapping("/login")
+    @GetMapping("/loginHome")
+    public String loginHome(){
+
+        return "loginHome";
+    }
+
+    @GetMapping("/loginGoogle")
     public String login(){
-        return "login";
+
+        return "loginGoogle";
     }
 }
