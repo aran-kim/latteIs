a7acb7d29499eff70ce965fe7391777891ef79fb
KimSangYub <ct06@naver.com>
2022-04-28 15:44:19 +0900

    0428-4

--- src/main/java/site/LatteIs/latteIs/web/domain/repository/PostRepository.java
+++ src/main/java/site/LatteIs/latteIs/web/domain/repository/PostRepository.java
@@ -12,6 +12,16 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Post findByBoardId(int board_id);
     List<Post> findAllByBoardId(int board_id);
 
+    @Query(value = "select * from post where board_id = ?1 order by create_date desc limit 3", nativeQuery = true)
+    List<Post> findThreeByBoardId(int board_id);
+
+
+    @Query(value = "select * from (\n" +
+            "\tselect *, rank() over(partition by board_id order by create_date asc) as ordernum\n" +
+            "    from post) as rankrow\n" +
+            "    where rankrow.ordernum <= 3", nativeQuery = true)
+    List<Post> findAllThreePost();
+
     @Override
     List<Post> findAll();
 
