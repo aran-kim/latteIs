140caa3375017bf980277b2691101c3e870535ff
aran <aran0317@naver.com>
2022-06-02 10:51:37 +0900

    STOMP 채팅 구현

--- src/main/java/site/LatteIs/latteIs/chat/WebSocketConfig.java
+++ src/main/java/site/LatteIs/latteIs/chat/WebSocketConfig.java
@@ -1,22 +1,24 @@
 package site.LatteIs.latteIs.chat;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@RequiredArgsConstructor
 @Configuration
-@EnableWebSocket //Websocket 활성화
-public class WebSocketConfig implements WebSocketConfigurer {
-
-    private final ChatHandler chatHandler;
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        /*Endpoint는 "/chat", 다른 서버에서도 접속이 가능하도록 setAllowedOrigin("*") 추가*/
-        registry.addHandler(chatHandler, "/ws/chat/chatting").setAllowedOrigins("https://*:8080");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/sub");
     }
 }
