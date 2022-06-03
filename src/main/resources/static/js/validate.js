557d4624506bdb17374944306e603eb7dff8e716
songyi <lsn21103@gmail.com>
2022-06-03 19:26:10 +0900

    회원가입 완성

---
+++ src/main/resources/static/js/validate.js
@@ -0,0 +1,89 @@
+/*변수 선언*/
+
+var id = document.querySelector('#id');
+
+var pw1 = document.querySelector('#pswd1');
+var pwMsg = document.querySelector('#alertTxt');
+var pwImg1 = document.querySelector('#pswd1_img1');
+
+var pw2 = document.querySelector('#pswd2');
+var pwMsgArea = document.querySelector('.int_pass');
+var userName = document.querySelector('#name');
+var email = document.querySelector('#email');
+var error = document.querySelectorAll('.error_next_box');
+
+
+
+/*이벤트 핸들러 연결*/
+
+id.addEventListener("focusout", checkId);
+pw1.addEventListener("focusout", checkPw);
+userName.addEventListener("focusout", checkName);
+email.addEventListener("focusout", isEmailCorrect);
+
+
+/*콜백 함수*/
+
+function checkId() {
+    var idPattern = /[a-zA-Z0-9_-]{5,20}/;
+    if(id.value === "") {
+        error[0].innerHTML = "필수 정보입니다.";
+        error[0].style.display = "block";
+    } else if(!idPattern.test(id.value)) {
+        error[0].innerHTML = "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.";
+        error[0].style.display = "block";
+    } else {
+        error[0].innerHTML = "사용 가능한 아이디입니다.";
+        error[0].style.color = "#08A600";
+        error[0].style.display = "block";
+    }
+}
+
+function checkPw() {
+    var pwPattern = /[a-zA-Z0-9~!@#$%^&*()_+|<>?:{}]{8,16}/;
+    if(pw1.value === "") {
+        error[1].innerHTML = "필수 정보입니다.";
+        error[1].style.display = "block";
+    } else if(!pwPattern.test(pw1.value)) {
+        error[1].innerHTML = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
+        pwMsg.innerHTML = "사용불가";
+        pwMsgArea.style.paddingRight = "93px";
+        error[1].style.display = "block";
+
+        pwMsg.style.display = "block";
+        pwImg1.src = "images/m_icon_not_use.png";
+    } else {
+        error[1].style.display = "none";
+        pwMsg.innerHTML = "안전";
+        pwMsg.style.display = "block";
+        pwMsg.style.color = "#03c75a";
+        pwImg1.src = "images/m_icon_safe.png";
+    }
+}
+
+function checkName() {
+    var namePattern = /[a-zA-Z가-힣]/;
+    if(userName.value === "") {
+        error[3].innerHTML = "필수 정보입니다.";
+        error[3].style.display = "block";
+    } else if(!namePattern.test(userName.value) || userName.value.indexOf(" ") > -1) {
+        error[3].innerHTML = "한글과 영문 대 소문자를 사용하세요. (특수기호, 공백 사용 불가)";
+        error[3].style.display = "block";
+    } else {
+        error[3].style.display = "none";
+    }
+}
+
+function isEmailCorrect() {
+    var emailPattern = /[a-z0-9]{2,}@[a-z0-9-]{2,}\.[a-z0-9]{2,}/;
+
+    if(email.value === ""){
+        error[6].style.display = "none";
+    } else if(!emailPattern.test(email.value)) {
+        error[6].style.display = "block";
+    } else {
+        error[6].style.display = "none";
+    }
+
+}
+
