88b9896db461f04819269553810458beb328f9e2
ksy <ct06@naver.com>
2022-06-16 15:16:15 +0900

    0616-1-oauth2 test18/google end

--- src/main/java/site/LatteIs/latteIs/web/domain/config/WebConfig.java
+++ src/main/java/site/LatteIs/latteIs/web/domain/config/WebConfig.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.view.MustacheViewResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import site.LatteIs.latteIs.auth.LoginUserArgumentResolver;
@@ -33,6 +35,15 @@ public class WebConfig implements WebMvcConfigurer {
         resolver.setSuffix(".html");
 
         registry.viewResolver(resolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/message/**")
+                .allowedOrigins("*")
+                .allowedMethods(HttpMethod.POST.name())
+                .allowCredentials(false)
+                .maxAge(3600);
     }
 
 }
