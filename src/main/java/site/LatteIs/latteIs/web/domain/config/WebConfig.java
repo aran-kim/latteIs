e987d41d1e279e6e48868a45c42aed1cc3fcca58
ksy <ct06@naver.com>
2022-06-16 13:13:01 +0900

    0616-1-oauth2 test4/google end

--- src/main/java/site/LatteIs/latteIs/web/domain/config/WebConfig.java
+++ src/main/java/site/LatteIs/latteIs/web/domain/config/WebConfig.java
@@ -1,10 +1,12 @@
 package site.LatteIs.latteIs.web.domain.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.view.MustacheViewResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import site.LatteIs.latteIs.auth.LoginUserArgumentResolver;
 
@@ -19,6 +21,18 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
         argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        MustacheViewResolver resolver = new MustacheViewResolver();
+
+        resolver.setCharset("UTF-8");
+        resolver.setContentType("text/html;charset=UTF-8");
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+
+        registry.viewResolver(resolver);
     }
 
 }
